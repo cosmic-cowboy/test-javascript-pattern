00b2747523056e91e33916c69fcfed39c128fa44
hiroo takaku <slgerkamp@gmail.com>
2013-06-01 17:23:49 +0900

    4.2.1 Callback Pattern 02 apply callback

--- 04-Functions/src/function.js
+++ 04-Functions/src/function.js
@@ -1,14 +1,34 @@
 // 4.2.1 コールバックの例
 
 // DOMツリーを巡回し、ページ要素を配列で返す
+// callbackを受け付ける
 
-var findNodes = function  () {
+var findNodes = function (callback) {
 
 	var i = 1000000,
 		nodes = [],
 		found;
+
+	// callbackできるか、確認
+	if(typeof callback !== "function"){
+		callback = false;
+	}
+
 	while(i){
 		i -= 1;
+
+		// dummy data
+		found = {
+					style : {
+						display : "inline",
+						color : "white"
+					}
+				};
+
+		// callback
+		if(callback){
+			callback(found);
+		}
 
 		nodes.push(found);
 	}
@@ -16,13 +36,8 @@ var findNodes = function  () {
 };
 
 
-var hide = function(nodes){
-	var i = 0,
-		max = nodes.length;
-	for(; i < max; i += 1) {
-		nodes[i].style.display = "node";
-	}
-};
+var hide = function(node){
 
-// findNodesでループし、hideでループする
-hide(findNodes());
+	node.style.display = "none";
+
+};
---
+++ 04-Functions/test/function_test.js
@@ -0,0 +1,6 @@
+TestCase("04-Funcitons Test",{
+	"test callback hide node" : function () {
+		// ノードが見つかり次第、hideを実行
+		findNodes(hide);
+	}
+});
