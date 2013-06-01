a44d8da6113393c2b75e723302f10ea022957979
hiroo takaku <slgerkamp@gmail.com>
2013-06-01 17:25:59 +0900

    4.2.1 Callback Pattern 03 anonymous callback

--- 04-Functions/test/function_test.js
+++ 04-Functions/test/function_test.js
@@ -2,5 +2,11 @@ TestCase("04-Funcitons Test",{
 	"test callback hide node" : function () {
 		// ノードが見つかり次第、hideを実行
 		findNodes(hide);
+	},
+	"test callback anonymous callback" : function () {
+
+		findNodes(function(node){
+			node.style.display = "block";
+		});
 	}
 });
