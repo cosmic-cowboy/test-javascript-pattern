4c537a801864e8629c4088a80ea918c3168a26c5
hiroo takaku <slgerkamp@gmail.com>
2013-06-02 09:13:23 +0900

    4.7 Init-Time Branching after

--- 04-Functions/src/utils.js
+++ 04-Functions/src/utils.js
@@ -1,20 +1,39 @@
 // 4.7 Init-Time Branching
 // utils.addListener()が呼ばれるたび、同じ検索が実行される
-// 初期化時分岐を使えばスクリプトが読み込まれる際の1書いだけで済む
 
 var utils = {
-	addListener : function (el, type, fn) {
-		if(typeof window.addListener === 'function'){
-			el.addEventListener(type, fn, false);
-		} else if (typeof document.attachEvent === 'function'){
-			// IE
-			el.attachEvent('on' + type, fn);
-		} else {
-			// older browsers
-			el['on' + type] = fn;
-		}
-	},
-	removeListener : function (el, type, fn) {
-		//
-	}
+	addListener : null,
+	removeListener : null
 };
+
+// 実装
+// 初期化時分岐を使えばスクリプトが読み込まれる際の1書いだけで済む
+
+if(typeof window.addListener === 'function'){
+
+	utils.addListener = function (el, type, fn){
+		el.addEventListener(type, fn, false);
+	};
+	utils.removeListener = function (el, type, fn){
+		el.removeEventListener(type, fn, false);
+	};
+
+} else if (typeof document.attachEvent === 'function'){
+	// IE
+	utils.addListener = function (el, type, fn){
+		el.attachEvent('on' + type, fn);
+	};
+	utils.removeListener = function (el, type, fn){
+		el.detachEvent('on' + type, fn);
+	};
+
+} else {
+	// older browsers
+	utils.addListener = function (el, type, fn){
+		el['on' + type] = fn;
+	};
+	utils.removeListener = function (el, type, fn){
+		el['on' + type] = null;
+	};
+
+}
