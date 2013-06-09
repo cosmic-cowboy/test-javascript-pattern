07a8aa3d5f230336faab8bc17038981081740053
hiroo takaku <slgerkamp@gmail.com>
2013-06-09 16:54:00 +0900

    06-Code-Reuse-Patterns 6.2 Classical Inheritancd Following the Prototype Chain

--- 06-Code-Reuse-Patterns/test/classical_Inheritance_test.js
+++ 06-Code-Reuse-Patterns/test/classical_Inheritance_test.js
@@ -1,9 +1,14 @@
 //6.2 Classical Inheritance
 // Expected Outcome When Using Classical Inheritance
 
-TestCase("06-Code-Reuse-Patterns Classical Inheritance", {
-	"test Classical Inheritance" : function () {
+TestCase("06-Code-Reuse-Patterns Classical Inheritance ", {
+	"test Classical Inheritance Following the Prototype Chain" : function () {
 		var kid = new Child();
+		assertEquals("Adam", kid.say());
+
+		kid.name = "Patrick";
+		assertEquals("Patrick", kid.say());
+		delete kid.name;
 		assertEquals("Adam", kid.say());
 	}
 });
