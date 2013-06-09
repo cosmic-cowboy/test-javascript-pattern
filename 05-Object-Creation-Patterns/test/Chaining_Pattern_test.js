33e9e7fa6d6eb130c021e807aef44d5f83ffe3b6
hiroo takaku <slgerkamp@gmail.com>
2013-06-09 15:11:31 +0900

    05-Object-Creation-Patterns Chaining Pattern revised

--- 05-Object-Creation-Patterns/test/Chaining_Pattern_test.js
+++ 05-Object-Creation-Patterns/test/Chaining_Pattern_test.js
@@ -4,6 +4,11 @@ TestCase("05-Object-Creation-Patterns Chaining Pattern", {
 
 	"test Chaining Pattern" : function () {
 		assertEquals(5, chaining_Pattern.increment().add(3).shout());
+
+		chaining_Pattern.value = 1;
+		chaining_Pattern.increment();
+		chaining_Pattern.add(7);
+		assertEquals(9, chaining_Pattern.shout());
 	}
 });
 
