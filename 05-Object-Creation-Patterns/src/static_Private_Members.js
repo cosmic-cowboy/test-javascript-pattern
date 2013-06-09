28963e8548061aa341829bdae35a99081d6f6c55
hiroo takaku <slgerkamp@gmail.com>
2013-06-09 14:34:19 +0900

    05-Object-Creation-Patterns Private Static Members adds a privileged method to access the static private property

--- 05-Object-Creation-Patterns/src/static_Private_Members.js
+++ 05-Object-Creation-Patterns/src/static_Private_Members.js
@@ -3,11 +3,27 @@ var Gadget_Private_Static = (function () {
 	
 	// 静的変数プロパティ
 	// static variable/property
-	var counter = 0;
+	var counter = 0,
+		ReturnObject;
 
 	// コンストラクタの新しい実装
 	// returning the new implementation of the constructor
-	return function () {
-		return counter += 1;
+	ReturnObject = function () {
+		counter += 1;
 	};
+
+	// 特権メソッド
+	ReturnObject.prototype.getLastId = function() {
+		return counter;
+	};
+
+	// 特権メソッド（クリア用 インスタンス生成時に追加されるのがかっこ悪いが…）
+	ReturnObject.prototype.clear = function() {
+		counter = 0;
+		return counter;
+	};
+	// コンストラクタに上書き
+	return ReturnObject;
+
 }());
+// execute immediately
--- 05-Object-Creation-Patterns/test/Static_Private_Members_test.js
+++ 05-Object-Creation-Patterns/test/Static_Private_Members_test.js
@@ -2,13 +2,20 @@ TestCase("05-Object-Creation-Patterns Private Static Members", {
 
 	"test Private Static Members" : function () {
 
-		// メソッドであれば値がとれるが、
-		var static_method = Gadget_Private_Static();
-		assertEquals("number", typeof static_method);
+		// // 5.6.2 Private Static Members
+		// // メソッドであれば値がとれるが、
+		// var static_method = Gadget_Private_Static();
+		// assertEquals("number", typeof static_method);
 
-		// インスタンスで返される値の中には数字はない（private member）
-		var instanceOfGadget_Private_Static = new Gadget_Private_Static();
-		assertEquals("object", typeof instanceOfGadget_Private_Static);
+		// // インスタンスで返される値の中には数字はない（private member）
+		// var instanceOfGadget_Private_Static = new Gadget_Private_Static();
+		// assertEquals("object", typeof instanceOfGadget_Private_Static);
+
+		assertEquals(0, new Gadget_Private_Static().clear());
+		assertEquals(1, new Gadget_Private_Static().getLastId());
+		assertEquals(2, new Gadget_Private_Static().getLastId());
+		assertEquals(3, new Gadget_Private_Static().getLastId());
+
 	}
 
 });
