ea88c1254b51a7a5a82c583dde32d613da241f23
hiroo takaku <slgerkamp@gmail.com>
2013-06-09 20:02:20 +0900

    06-Code-Reuse-Patterns Classical Pattern #3 Rent and Set Prototype

--- 06-Code-Reuse-Patterns/test/classical_Inheritance_test.js
+++ 06-Code-Reuse-Patterns/test/classical_Inheritance_test.js
@@ -1,7 +1,7 @@
 //6.2 Classical Inheritance
 // Expected Outcome When Using Classical Inheritance
-
-TestCase("06-Code-Reuse-Patterns Classical Inheritance The Default Pattern", {
+// 6.3 クラシカルなパターンその１：デフォルトパターン
+TestCase("06-Code-Reuse-Patterns Classical Pattern #1 The Default Pattern", {
 
 	"test Classical Inheritance Following the Prototype Chain" : function () {
 
@@ -43,28 +43,18 @@ TestCase("06-Code-Reuse-Patterns Classical Inheritance The Default Pattern", {
 
 		function inherit(C, P) {
 
-			// prototypeプロパティは関数ではなく、オブジェクトを指す
-			// コンストラクタ自体ではなく、親コンストラクタを生成したインスタンスを指すようにする
-			// new を使用し、インスタンスを生成すると、Pのインスタンスの機能がプロトタイプを介して、利用できるようになる
-			// thisに追加したインスタンス固有のプロパティとプロトタイプのプロパティが継承される
-
 			C.prototype = new P();
 		}
 
-		// the parent constructor
 		function Parent(name) {
 			this.name = name || "Adam";
 		}
-
-		// adding functionality to the prototype
 		Parent.prototype.say = function() {
 			return this.name;
 		};
 
-		//6.2 Classical Inheritance Child
 		function Child(name) {}
 
-		// inheritance magic happens here
 		inherit(Child, Parent);
 
 		var s_kid = new Child("Seth");
@@ -72,8 +62,8 @@ TestCase("06-Code-Reuse-Patterns Classical Inheritance The Default Pattern", {
 	}
 });
 
-
-TestCase("06-Code-Reuse-Patterns Classical Inheritance Rent-a-Constructor", {
+// 6.4 クラシカルなパターンその２：コンストラクタ拝借
+TestCase("06-Code-Reuse-Patterns Classical Pattern #2 Rent a Constructor", {
 
 	"test Classical Inheritance Rent-a-Constructor" : function () {
 		// a parent constructor
@@ -125,3 +115,72 @@ TestCase("06-Code-Reuse-Patterns Classical Inheritance Rent-a-Constructor", {
 		assertEquals("undefined", typeof kid.say);
 	}
 });
+
+
+// 6.5 クラシカルなパターンその３　クラシカルなパターンその３　プロトタイプを拝借して設定する
+TestCase("06-Code-Reuse-Patterns Classical Pattern #3 Rent and Set Prototype", {
+
+	"test Classical Inheritance Rent and Set Prototype" : function () {
+		function inherit(C, P) {
+			C.prototype = new P();
+		}
+
+		function Parent(name) {
+			this.name = name || "Adam";
+		}
+		Parent.prototype.say = function() {
+			return this.name;
+		};
+
+		function Child(name) {
+			Parent.apply(this,arguments);
+		}
+		Child.prototype = new Parent();
+
+		var kid = new Child("Patrick");
+		assertEquals("Patrick",   kid.name);
+		assertEquals("Patrick",   kid.say());
+		delete kid.name;
+		assertEquals("Adam",   kid.say());
+
+	}
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
