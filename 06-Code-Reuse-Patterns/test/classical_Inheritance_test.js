d7b76f72432d87c7549f4f25d349891e13490c71
hiroo takaku <slgerkamp@gmail.com>
2013-06-09 17:21:47 +0900

    06-Code-Reuse-Patterns 6.4 Classical Inheritancd Rent a Constructor

--- 06-Code-Reuse-Patterns/test/classical_Inheritance_test.js
+++ 06-Code-Reuse-Patterns/test/classical_Inheritance_test.js
@@ -1,7 +1,7 @@
 //6.2 Classical Inheritance
 // Expected Outcome When Using Classical Inheritance
 
-TestCase("06-Code-Reuse-Patterns Classical Inheritance ", {
+TestCase("06-Code-Reuse-Patterns Classical Inheritance The Default Pattern", {
 	"test Classical Inheritance Following the Prototype Chain" : function () {
 		var kid = new Child();
 		assertEquals("Adam", kid.say());
@@ -14,5 +14,40 @@ TestCase("06-Code-Reuse-Patterns Classical Inheritance ", {
 	"test Classical Inheritance Drawbacks When Using Pattern #1" : function () {
 		var s_kid = new Child("Seth");
 		assertEquals("Adam", s_kid.say());
+	}
+});
+
+
+TestCase("06-Code-Reuse-Patterns Classical Inheritance Rent-a-Constructor", {
+	"test Classical Inheritance Rent-a-Constructor" : function () {
+		// a parent constructor
+		function Article () {
+			this.tags = ["js", "css"];
+		}
+
+		var article = new Article();
+
+		// a blog post inherits from an article object
+		function BlogPost () {}
+		BlogPost.prototype = article;
+		var blog = new BlogPost();
+
+		// コンストラクタ拝借パターンでArticleからの継承
+		function StaticPage () {
+			Article.call(this);
+		}
+		var page = new StaticPage();
+
+		assertTrue(article.hasOwnProperty("tags"));
+		assertFalse(blog.hasOwnProperty("tags"));
+		assertTrue(page.hasOwnProperty("tags"));
+
+		// tagsを共有している
+		blog.tags.push("html");
+		// 固有のtagsプロパティを持つ
+		page.tags.push("php");
+		assertEquals("js, css, html", article.tags.join(', '));
+		assertEquals("js, css, html", blog.tags.join(', '));
+		assertEquals("js, css, php", page.tags.join(', '));
 	}
 });
